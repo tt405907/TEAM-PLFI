91d01019269b049b27372dad214f81ddd532c8b3
tt405907 <theo.tracol@etu.univ-cotedazur.fr>
2019-02-13 18:14:36 +0100

    Update Android

--- android/app/src/main/java/plfi/plfi/MainActivity.java
+++ android/app/src/main/java/plfi/plfi/MainActivity.java
@@ -1,7 +1,12 @@
 package plfi.plfi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,9 +14,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.test1);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // init du RTC Game
+        Button ButtonStarterRTC = (Button)findViewById(R.id.buttonStartRTC);
+        ButtonStarterRTC.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View actuelView)    //au clic sur le bouton
+            {
+                Intent intent = new Intent(MainActivity.this, gameRTC.class);  //Lancer l'activité DisplayVue
+                startActivity(intent);    //Afficher la vue
+            }
+        });
+    }
+
+
+
+    //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
         //getSupportActionBar().setSubtitle("Using ToolBar");
     }
-}
+
