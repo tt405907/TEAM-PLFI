76edbce166c9e03d677e9c9e11c5e513d69626a2
tt405907 <theo.tracol@etu.univ-cotedazur.fr>
2019-02-13 16:39:56 +0100

    Refractor

--- android/app/src/main/java/plfi/plfi/MainActivity.java
+++ android/app/src/main/java/plfi/plfi/MainActivity.java
@@ -2,12 +2,16 @@ package plfi.plfi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.test1);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        //getSupportActionBar().setSubtitle("Using ToolBar");
     }
 }
