b542c89262b9b3eca1ac1a6d8a2b1944a3a40d6a
tt405907 <theo.tracol@etu.univ-cotedazur.fr>
2019-02-27 18:28:20 +0100

    Mise a jour

---
+++ android/app/src/main/java/commun/Identification.java
@@ -0,0 +1,21 @@
+package commun;
+
+public class Identification {
+
+    private String nom ;
+
+    public Identification() {}
+
+    public Identification(String nom) {
+        this.nom = nom;
+    }
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
--- android/app/src/main/java/plfi/plfi/gameRTCPrint.java
+++ android/app/src/main/java/plfi/plfi/gameRTCPrint.java
@@ -12,7 +12,15 @@ public class gameRTCPrint extends AppCompatActivity {
     // Mes boutons
     Button new_content;
     Button save_btn;
+    Button buttonCarre;
+    Button buttonTriangle;
+    Button buttonCercle;
+
+    // Notre canvas
     gamePRINT gamePrint;
+
+    // temporaire pour tester notre truck
+    int imageId;
 
     // Image à afficher pour le serveur
     ImageView imageViewServeur;
@@ -26,34 +34,76 @@ public class gameRTCPrint extends AppCompatActivity {
         //image du serveur
         imageViewServeur = (ImageView) findViewById(R.id.imageServeur);
 
+        // Clear notre Canvas
         new_content = (Button) findViewById(R.id.new_btn);
         new_content.setOnClickListener(new ButtonNewContent());
 
+        //Carre
+        buttonCarre = (Button) findViewById(R.id.buttonCarre);
+        buttonCarre.setOnClickListener(new ButtonCarreClick());
+
+        //Triangle
+        buttonTriangle = (Button) findViewById(R.id.buttonTriangle);
+        buttonTriangle.setOnClickListener(new ButtonTriangleClick());
+
+        //Cercle
+        buttonCercle = (Button) findViewById(R.id.buttonCercle);
+        buttonCercle.setOnClickListener(new ButtonCercleClick());
+
+        // Envoit au serveur
         save_btn = (Button) findViewById(R.id.save_btn);
         save_btn.setOnClickListener(new ButtonSave_Btn());
 
 
-
     }
-        // Nouvelle page
-        class ButtonNewContent implements View.OnClickListener{
 
-            @Override
-            public void onClick(View v){
-                gamePrint = (gamePRINT) findViewById(R.id.drawing);
-                gamePrint.reset();
-            }
+    // Nouvelle page
+    class ButtonNewContent implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            gamePrint = (gamePRINT) findViewById(R.id.drawing);
+            gamePrint.reset();
         }
-
-
-        // TODO: A finir ! !! TAOOOOOOOOOOOOOOO C'EST ICI POUR LE SERVEUR !!!!!!!!!!!!
-        class ButtonSave_Btn implements View.OnClickListener{
-
-            @Override
-            public void onClick(View v){
-
-            }
-        }
-
     }
+
+
+
+    //TODO: REFAIRE IMAGE CARRE
+    class ButtonCarreClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+            imageId = 3;
+        }
+    }
+
+    //TODO: REFAIRE L'IMAGE TRIANGLE
+    class ButtonTriangleClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            imageId = 2;
+        }
+    }
+
+    //TODO: REFAIRE L'IMAGE CERCLE
+    class ButtonCercleClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            imageId = 4;
+
+        }
+    }
+
+    // TODO: A finir ! !! TAOOOOOOOOOOOOOOO C'EST ICI POUR LE SERVEUR ! (remplace le bouton VALIDER) gros copie colle )
+    class ButtonSave_Btn implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+
+}
 
