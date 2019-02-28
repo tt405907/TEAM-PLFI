4d0a32cb42510571e18c61e7149fbb4c5aaadad4
tao <zhaopeng.tao@etu.unice.fr>
2019-02-28 14:13:44 +0100

    .

--- android/app/src/main/java/plfi/plfi/gamePRINT.java
+++ android/app/src/main/java/plfi/plfi/gamePRINT.java
@@ -5,33 +5,16 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-
+import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
 
-import java.util.ArrayList;
+import java.util.jar.Attributes;
+
 public class gamePRINT extends View {
-
-    public class Position {
-        float x;
-        float y;
-
-        public float getX() {
-            return x;
-        }
-        public float getY() {
-            return y;
-        }
-
-        public Position(float x, float y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-    public ArrayList<Position> tableauPositions;
-
+    
     Paint paint;
     Path path;
     public gamePRINT(Context context, AttributeSet attrs) {
@@ -43,7 +26,6 @@ public class gamePRINT extends View {
         paint.setStrokeJoin(Paint.Join.ROUND);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(5f);
-        tableauPositions = new ArrayList<>();
 
     }
 
@@ -58,7 +40,7 @@ public class gamePRINT extends View {
     public boolean onTouchEvent(MotionEvent event){
         float xPos = event.getX();
         float yPos = event.getY();
-        tableauPositions.add(new Position(xPos,yPos));
+
         switch (event.getAction())
         {
             case MotionEvent.ACTION_DOWN:
@@ -77,11 +59,7 @@ public class gamePRINT extends View {
     }
 
     public void reset(){
-        tableauPositions.clear();
         path.reset();
         invalidate();
-    }
-    public ArrayList<Position> getTableau() {
-        return tableauPositions;
     }
 }
--- android/app/src/main/java/plfi/plfi/gameRTCPrint.java
+++ android/app/src/main/java/plfi/plfi/gameRTCPrint.java
@@ -6,13 +6,46 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.util.Log;
 
-public class gameRTCPrint extends AppCompatActivity {
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import commun.Dessin;
+import commun.Forme;
+import commun.Identification;
+import commun.jeux.ResultCTR;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
+public class gameRTCPrint extends AppCompatActivity implements DisplayRTC {
 
     // Mes boutons
     Button new_content;
     Button save_btn;
+    Button buttonCarre;
+    Button buttonTriangle;
+    Button buttonCercle;
+
+    // Notre canvas
     gamePRINT gamePrint;
+    Connexion connexion;
+    Controleur controleur;
+
+    // temporaire pour tester notre truck
+    int imageId;
 
     // Image à afficher pour le serveur
     ImageView imageViewServeur;
@@ -23,37 +56,105 @@ public class gameRTCPrint extends AppCompatActivity {
 
         setContentView(R.layout.print);
 
-        //image du serveur
+        // image du serveur
         imageViewServeur = (ImageView) findViewById(R.id.imageServeur);
 
+        // Clear notre Canvas
         new_content = (Button) findViewById(R.id.new_btn);
         new_content.setOnClickListener(new ButtonNewContent());
 
+        // Carre
+        buttonCarre = (Button) findViewById(R.id.buttonCarre);
+        buttonCarre.setOnClickListener(new ButtonCarreClick());
+
+        // Triangle
+        buttonTriangle = (Button) findViewById(R.id.buttonTriangle);
+        buttonTriangle.setOnClickListener(new ButtonTriangleClick());
+
+        // Cercle
+        buttonCercle = (Button) findViewById(R.id.buttonCercle);
+        buttonCercle.setOnClickListener(new ButtonCercleClick());
+
+        // Envoit au serveur
         save_btn = (Button) findViewById(R.id.save_btn);
         save_btn.setOnClickListener(new ButtonSave_Btn());
-
-
-
-    }
-        // Nouvelle page
-        class ButtonNewContent implements View.OnClickListener{
-
-            @Override
-            public void onClick(View v){
-                gamePrint = (gamePRINT) findViewById(R.id.drawing);
-                gamePrint.reset();
-            }
-        }
-
-
-        // TODO: A finir ! !! TAOOOOOOOOOOOOOOO C'EST ICI POUR LE SERVEUR !!!!!!!!!!!!
-        class ButtonSave_Btn implements View.OnClickListener{
-
-            @Override
-            public void onClick(View v){
-
-            }
-        }
+        controleur = new Controleur(gameRTCPrint.this);
+        Connexion connexion = new Connexion("192.168.0.101", "10101", controleur);
+        connexion.seConnecter();
+        this.connexion = connexion;
 
     }
 
+    @Override
+    public void updateGame(String winner, final String img) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (img.equals("TRIANGLE")) {
+                    imageViewServeur.setImageResource(R.drawable.triangle);
+                } else if (img.equals("CARRE")) {
+                    imageViewServeur.setImageResource(R.drawable.carre);
+                } else if (img.equals("ROND")) {
+                    imageViewServeur.setImageResource(R.drawable.cercle);
+                } else {
+                    imageViewServeur.setImageResource(R.drawable.carre);
+                }
+            }
+        });
+    }
+
+    // Nouvelle page
+    class ButtonNewContent implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            gamePrint = (gamePRINT) findViewById(R.id.drawing);
+            gamePrint.reset();
+        }
+    }
+
+    // TODO: REFAIRE IMAGE CARRE
+    class ButtonCarreClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+            imageId = 3 ;
+        }
+    }
+
+    // TODO: REFAIRE L'IMAGE TRIANGLE
+    class ButtonTriangleClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            imageId = 2;
+        }
+    }
+
+    // TODO: REFAIRE L'IMAGE CERCLE
+    class ButtonCercleClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            imageId = 4;
+
+        }
+    }
+
+    // TODO: A finir ! !! TAOOOOOOOOOOOOOOO C'EST ICI POUR LE SERVEUR ! (remplace le
+    // bouton VALIDER) gros copie colle )
+    class ButtonSave_Btn implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            if (imageId == 2) {
+                connexion.sendForme(Forme.TRIANGLE.ordinal());
+            } else if (imageId == 3) {
+                connexion.sendForme(Forme.CARRE.ordinal());
+            } else if (imageId == 4) {
+                connexion.sendForme(Forme.ROND.ordinal());
+            } else {
+                connexion.sendForme(Forme.TRIANGLE.ordinal());
+            }
+        }
+    }
+
+}
