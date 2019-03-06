f11b3983308ece1707ee426c967c776cac8d0b9e
arbona arnaud <aa303554@etu.unice.fr>
2019-03-06 17:12:58 +0100

    Test cercle #15

--- javastd/serveur/src/test/java/serveur/FormeMatcherCercleTest.java
+++ javastd/serveur/src/test/java/serveur/FormeMatcherCercleTest.java
@@ -22,23 +22,28 @@ public class FormeMatcherCercleTest {
 	@Test
 	public void cercleParfait() {
 		Dessin cercle;
-		Point[] pointsCercle = new Point[360];
-		for (int i = 0; i < 360; i++) {
-			pointsCercle[i] = new Point((float) Math.cos(i * 2 * Math.PI / 360.0) * 100 + 250,
-					(float) Math.sin(i * 2 * Math.PI / 360.0) * 100 + 250);
+		Point[] pointsCercle = new Point[180];
+		for (int i = 0; i < 180; i++) {
+			pointsCercle[i] = new Point((float) Math.cos(i * 2 * Math.PI / 180.0) * 50 + 125,
+					(float) Math.sin(i * 2 * Math.PI / 180.0) * 50 + 125);
 		}
 		cercle = new Dessin(pointsCercle);
 		assertEquals(Forme.ROND, fm.identify(cercle));
 	}
 
 	@Test
-	public void translationCarre() {
-		Dessin carre = new Dessin(new Point[] {new Point(200, 200), new Point(400, 200), new Point(400, 400), new Point(200, 400)});
-		
+	public void translationCercle() {
+		Dessin cercle;
+		Point[] pointsCercle = new Point[180];
+		for (int i = 0; i < 180; i++) {
+			pointsCercle[i] = new Point((float) Math.cos(i * 2 * Math.PI / 180.0) * 50 + 125,
+					(float) Math.sin(i * 2 * Math.PI / 180.0) * 50 + 125);
+		}
+		cercle = new Dessin(pointsCercle);
 		//Translate dans pleins de directions
 		for (int x = -200; x <= 200; x += 200) {
 			for (int y = -200; y <= 200; y += 200) {
-				assertEquals(Forme.CARRE, fm.identify(translate(carre, x, y)));
+				assertEquals(Forme.ROND, fm.identify(translate(cercle, x, y)));
 			}
 		}
 		
