dfb048bf00c67233129130dc83ee5a1916ec2b3b
ThomasPortet <44069601+ThomasPortet@users.noreply.github.com>
2019-04-17 18:12:21 +0200

    Ajout test bouton retour Game Reflex

--- android/app/src/androidTest/java/plfi/plfi/GameReflexTest.java
+++ android/app/src/androidTest/java/plfi/plfi/GameReflexTest.java
@@ -7,6 +7,7 @@ import android.view.ViewParent;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.AllOf;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -16,8 +17,13 @@ import androidx.test.filters.LargeTest;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
@@ -35,6 +41,8 @@ public class GameReflexTest {
 
     @Test
     public void ButtonStart() {
+        // On vérifie d'abord que le bouton est activé
+        assertTrue(mainActivityRule.getActivity().btn_start.isEnabled());
         ViewInteraction appCompatButton2 = onView(
                 allOf(withId(R.id.start_reflex), withText("START"),
                         childAtPosition(
@@ -44,7 +52,23 @@ public class GameReflexTest {
                                 0),
                         isDisplayed()));
         appCompatButton2.perform(click());
+        // Comme le serveur est deconnecté dans le cadre de ce test, appuyer sur le bouton start devrait le désactiver
         assertFalse(mainActivityRule.getActivity().btn_start.isEnabled());
+    }
+
+    @Test
+    public void ReturnButton() {
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Revenir en haut de la page"),
+                        childAtPosition(
+                                allOf(withId(R.id.toobar_reflex),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+        intended(AllOf.allOf(hasComponent(hasShortClassName(".MainActivity")), toPackage(PACKAGE_NAME)));
     }
 
     private static Matcher<View> childAtPosition(
